eac39fcc579ba3e6ffec9962ee22973c983530d5
callmesophiii <callmesophiii@gmail.com>
2025-07-24 20:27:36 -0400

    Update SignIn.jsx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,74 +1,53 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { backendClient } from "../clients/backendClient";
-
+import { useNavigate } from "react-router-dom";
 function SignInPage() {
-  const navigate = useNavigate();
-
+    const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    username: "",
+    email: "",
     password: "",
   });
-
-  const [error, setError] = useState("");
-
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError("");
-
     try {
-      const response = await backendClient.post("/users/login", formData);
-      console.log("Login success:", response.data);
-
-      localStorage.setItem("token", response.data.token);
-
-      navigate("/");
-    } catch (err) {
-      console.error("Login failed:", err);
-      setError("Invalid username or password.");
+        const res = await backendClient.post('/users/login', formData);
+        console.log(res.data);
+        localStorage.setItem('social-app-token', JSON.stringify(res.data.token))
+        navigate('/feed');
+    } catch (error) {
+        console.log(error);
     }
   };
-
   return (
     <main>
-      <h1>Sign In</h1>
-
+      <h1>Sign In Page</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
+        <h2>Sign In</h2>
+        <label htmlFor="email" />
         <input
-          type="text"
-          id="username"
-          name="username"
-          placeholder="Enter username"
-          autoComplete="username"
-          value={formData.username}
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
           onChange={handleChange}
         />
-
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password" />
         <input
           type="password"
-          id="password"
           name="password"
-          placeholder="Enter password"
-          autoComplete="current-password"
+          placeholder="Password"
           value={formData.password}
           onChange={handleChange}
         />
-
         <input type="submit" value="Login" />
       </form>
-
-      {error && <p style={{ color: "red" }}>{error}</p>}
     </main>
   );
 }
-
 export default SignInPage;
